144f9e8353c89bb810540b725c70a78ae4208e5a
Anabel Davila <juanislala2@gmail.com>
2025-03-04 22:44:20 -0500

    Actualizacion medicos

---
+++ Capa Negocio/MedicoBL.cs
@@ -0,0 +1,46 @@
+﻿using Capa_Datos;
+using Capa_Entidad;
+
+namespace Capa_Negocio
+
+{
+    public class MedicoBL
+    {
+        public List<MedicosCLS> ListarMedicos()
+        {
+
+            MedicosDAL obj = new MedicosDAL();
+            return obj.ListarMedicos();
+        }
+
+        public int GuardarMedicos(MedicosCLS oMedicoCLS)
+        {
+            ValidarMedico(oMedicoCLS);
+            MedicosDAL obj = new MedicosDAL();
+            return obj.GuardarMedico(oMedicoCLS);
+        }
+        private void ValidarMedico(MedicosCLS medico)
+        {
+            if (medico.nombre == null)
+            {
+                throw new Exception("El nombre no puede ser nulo");
+            }
+            if (medico.apellido == null)
+            {
+                throw new Exception("El apellido no puede ser nulo");
+            }
+            if (medico.especialidadId == 0)
+            {
+                throw new Exception("La especialidad no puede ser nula");
+            }
+            if (string.IsNullOrEmpty(medico.telefono) || !System.Text.RegularExpressions.Regex.IsMatch(medico.telefono, "^09\\d{8}$"))
+            {
+                throw new ArgumentException("El teléfono debe tener 10 dígitos y empezar con '09'.");
+            }
+            if (medico.email == null)
+            {
+                throw new Exception("El email no puede ser nulo");
+            }
+        }
+    }
+}
